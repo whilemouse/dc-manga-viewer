d650fc51e4a89fa407bb1eca83966df272f27ad3
jay <whilemouse@gmail.com>
2024-09-14 21:52:11 +0900

    fix: 기본 틀 완성

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,11 +1,52 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 
-const pIndex = ref(1)
+const pIndex = ref(0)
 const isOpen = ref(false)
+let imagesArray: HTMLImageElement[] = []
+
+onMounted(() => {
+  const writeDiv = document.querySelector('.write_div')
+  const images = writeDiv!.querySelectorAll('img')
+  imagesArray = Array.from(images)
+})
+
+onUnmounted(() => {
+})
+
+// 키보드 이벤트 핸들러 함수
+function handleKeyDown(event: KeyboardEvent) {
+  if (event.key === 'ArrowRight') {
+    pageNext()
+  }
+  if (event.key === 'ArrowLeft') {
+    pagePrev()
+  }
+  if (event.key === 'Escape') {
+    closeViewer()
+  }
+}
 
 function openViewer() {
   isOpen.value = true
+  window.addEventListener('keydown', handleKeyDown)
+}
+
+function closeViewer() {
+  isOpen.value = false
+  window.removeEventListener('keydown', handleKeyDown)
+}
+
+function pagePrev() {
+  if (pIndex.value === 0)
+    return
+  pIndex.value -= 1
+}
+
+function pageNext() {
+  if (pIndex.value === imagesArray.length - 1)
+    return
+  pIndex.value += 1
 }
 </script>
 
@@ -14,13 +55,18 @@ function openViewer() {
     <button class="btn btn-secondary" @click="openViewer">
       뷰어 열기
     </button>
+
     <dialog v-if="isOpen" class="h-full bg-black modal" open>
-      <img v-show="pIndex === 1" src="https://dcimg1.dcinside.com/viewimage.php?id=2eb2dd2fe6ed36a379ee&no=29bcc427bd8077a16fb3dab004c86b6f24b7e12241ba1d97df86ebc326b431b1d575f47ffe1a641706fbce169a0d8acfea2f66830080d0d5f0378732cbbd9b15" alt="">
-      <img v-show="pIndex === 2" src="https://dcimg1.dcinside.com/viewimage.php?id=2eb2dd2fe6ed36a379ee&no=29bcc427bd8077a16fb3dab004c86b6f24b7e12241ba1d97df86ebc326b431b1d575f47ffe1a641706fbce169a0d8acfea2f668305828186ad33d732cbbd9b15" alt="">
-      <img v-show="pIndex === 3" src="https://dcimg1.dcinside.com/viewimage.php?id=2eb2dd2fe6ed36a379ee&no=29bcc427bd8077a16fb3dab004c86b6f24b7e12241ba1d97df86ebc326b431b1d575f47ffe1a641706fbce169a0d8acfea2f66830381d4d5f0328937cbbd9b15" alt="">
-      <button class="fixed text-2xl left-5" @click="() => pIndex += 1">
+      <img v-for="(image, index) in imagesArray" v-show="pIndex === index" :key="index" class="h-full" :src="image.src" alt="">
+      <button class="fixed text-5xl left-5" @click="pagePrev">
+        <
+      </button>
+      <button class="fixed text-5xl right-5" @click="pageNext">
         >
       </button>
+      <!-- <button class="fixed top-0 text-2xl left-1/2" @click="closeViewer">
+        X
+      </button> -->
     </dialog>
   </div>
 </template>
