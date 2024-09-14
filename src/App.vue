edb03e8976b272b809793299b4a92bb1bce37578
jay <whilemouse@gmail.com>
2024-09-14 21:15:31 +0900

    feature: 뷰어 추가

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import logo from './assets/logo.png'
-
-declare const chrome: any // Declare 'chrome' as a global variable
+import TheTest from './components/TheTest.vue'
 
 const logoUrl = chrome.runtime.getURL(logo)
 </script>
 
 <template>
   <div class="my-extension ">
-    <a href="b" class=" text-3xl">A23</a>
+    <a href="b" class="">21A23</a>
+    <button class="btn btn-secondary">
+      Button
+    </button>
+    <TheTest />
   </div>
   <img alt="Vue logo" :src="logoUrl">
 </template>
---
+++ src/components/TheTest.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const pIndex = ref(1)
+const isOpen = ref(false)
+
+function openViewer() {
+  isOpen.value = true
+}
+</script>
+
+<template>
+  <div>
+    <button class="text-7xl btn" @click="openViewer">
+      뷰어 열기
+    </button>
+    <dialog v-if="isOpen" class="h-full bg-black modal" open>
+      <img v-show="pIndex === 1" src="https://dcimg1.dcinside.com/viewimage.php?id=2eb2dd2fe6ed36a379ee&no=29bcc427bd8077a16fb3dab004c86b6f24b7e12241ba1d97df86ebc326b431b1d575f47ffe1a641706fbce169a0d8acfea2f66830080d0d5f0378732cbbd9b15" alt="">
+      <img v-show="pIndex === 2" src="https://dcimg1.dcinside.com/viewimage.php?id=2eb2dd2fe6ed36a379ee&no=29bcc427bd8077a16fb3dab004c86b6f24b7e12241ba1d97df86ebc326b431b1d575f47ffe1a641706fbce169a0d8acfea2f668305828186ad33d732cbbd9b15" alt="">
+      <img v-show="pIndex === 3" src="https://dcimg1.dcinside.com/viewimage.php?id=2eb2dd2fe6ed36a379ee&no=29bcc427bd8077a16fb3dab004c86b6f24b7e12241ba1d97df86ebc326b431b1d575f47ffe1a641706fbce169a0d8acfea2f66830381d4d5f0328937cbbd9b15" alt="">
+      <button class="fixed text-2xl left-5" @click="() => pIndex += 1">
+        >
+      </button>
+    </dialog>
+  </div>
+</template>
