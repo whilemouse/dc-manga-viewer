754da1857ef203765b11df5f6a8c9b8ac825a142
jay <whilemouse@gmail.com>
2024-09-14 21:27:07 +0900

    fix: add dcincide

--- src/content.js
+++ src/content.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import './style.css'
 
 const root = document.createElement('div')
-root.id = 'crx-root'
-document.body.prepend(root)
+root.id = 'manga-viewer'
+
+const content = document.querySelector('.gallview_contents')
+if (content) {
+  content.prepend(root)
+}
+else {
+  document.body.prepend(root)
+}
 
 const app = createApp(App)
 app.mount(root)
