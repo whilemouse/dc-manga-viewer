1b7b0f0b94f8b886e1cb2b42b771363d19185955
jay <whilemouse@gmail.com>
2024-09-14 14:30:46 +0900

    feature: add content script

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import logo from './assets/logo.png';
+const logoUrl = chrome.runtime.getURL(logo);
+
+
+
+console.log('App')
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" :src="logoUrl" />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#crx-root {
+  position: fixed;
+  top: 3rem;
+  left: 50%;
+  transform: translate(-50%, 0);
 }
-</style>
+
+#crx-root button {
+  background-color: rgb(239, 239, 239);
+  border-color: rgb(118, 118, 118);
+  border-image: initial;
+  border-style: outset;
+  border-width: 2px;
+  margin: 0;
+  padding: 1px 6px;
+}</style>
---
+++ src/content.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const root = document.createElement('div')
+root.id = 'crx-root'
+document.body.append(root)
+
+const app = createApp(App)
+app.mount(root)
