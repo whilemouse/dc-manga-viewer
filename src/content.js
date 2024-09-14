2dc9c3e4a95b1c58adf19c18d1b6f24ad1069fd8
jay <whilemouse@gmail.com>
2024-09-14 22:03:13 +0900

    -

--- src/content.js
+++ src/content.js
@@ -9,9 +9,6 @@ const content = document.querySelector('.gallview_contents')
 if (content) {
   content.prepend(root)
 }
-else {
-  document.body.prepend(root)
-}
 
 const app = createApp(App)
 app.mount(root)
