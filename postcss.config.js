f1c07b40bce7e45d0f70d3b4695a1ae4e937a193
jay <whilemouse@gmail.com>
2024-09-14 16:04:20 +0900

    chore: postcss-prefix-selector를 통해 tailwind를 my-extensions 클래스 밑에만 적용

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+// postcss.config.js
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-prefix-selector')({
+      prefix: '.my-extension',
+      exclude: ['.my-extension'], // 이미 프리픽스가 있는 선택자는 제외
+    }),
+  ],
+};
