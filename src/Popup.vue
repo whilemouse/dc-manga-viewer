4bbff9e6afbf5eff395e43a90bf355c5bac74744
jay <whilemouse@gmail.com>
2024-09-16 11:34:44 +0900

    feature: init popup

---
+++ src/Popup.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+console.log('11')
+</script>
+
+<template>
+  <div class="my-extension">
+    <div class="mt-10 text-5xl ">
+      Popup vue
+    </div>
+  </div>
+</template>
