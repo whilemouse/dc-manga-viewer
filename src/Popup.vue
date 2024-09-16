d9ca71eb62f43d887f6985b456b31744ec464d5e
jay <whilemouse@gmail.com>
2024-09-16 12:06:18 +0900

    fix: -

--- src/Popup.vue
+++ src/Popup.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-console.log('11')
+import ViewerIcon from './assets/svg/ViewerIcon.vue'
 </script>
 
 <template>
   <div class="my-extension">
-    <div class="mt-10 text-5xl ">
-      Popup vue
+    <div class="w-40">
+      <h1 class="text-2xl">
+        만갤 뷰어
+      </h1>
+      <div>
+        <div class="mt-1">
+          <p class="flex items-center ">
+            뷰어: <ViewerIcon class="w-5" />
+          </p>
+          <p>다음 페이지: →↓ </p>
+          <p>이전 페이지: ←↑  </p>
+          <p>댓글로 이동: /  </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
